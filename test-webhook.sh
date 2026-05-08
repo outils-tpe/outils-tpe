060d54dcb5aa1ea9eefafe958bfedd2b810e64f3
thomas_solumatic <thomas@solumatic.fr>
2026-05-08 11:21:22 +0200

    Sauvegarde auto avant modification

--- test-webhook.sh
+++ test-webhook.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-stripe trigger checkout.session.completed --override "checkout_session:metadata.file_key=tresorerie-electricien.xlsx" --override "checkout_session:metadata.product_slug=tresorerie-electricien" --override "checkout_session:customer_details.email=achatsthomas@gmail.com"
+stripe trigger checkout.session.completed --override "checkout_session:metadata.file_key=tresorerie-electricien.xlsx" --override "checkout_session:metadata.product_slug=tresorerie-electricien" --override "checkout_session:customer_email=achatsthomas@gmail.com"
